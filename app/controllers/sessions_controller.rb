1e895311286ce6e446321cdb18aeffb5653b334e
RALPH <ralph@RALPHnoMacBook-Air.local>
2026-02-10 12:12:41 +0900

    logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = login(params[:email], params[:password])
+    if user
+      redirect_to root_path
+    else
+      flash.now[:alert] = "メールアドレスかパスワードが違います"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    logout
+    redirect_to login_path
   end
 end
