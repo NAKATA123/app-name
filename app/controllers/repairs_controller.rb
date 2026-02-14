6e2a126f70095ba3b6dd73380911188f45cf3949
RALPH <ralph@RALPHnoMacBook-Air.local>
2026-02-14 14:41:45 +0900

    repair

--- app/controllers/repairs_controller.rb
+++ app/controllers/repairs_controller.rb
@@ -6,4 +6,26 @@ class RepairsController < ApplicationController
   def show
     @repair = Repair.includes(car: :customer).find(params[:id])
   end
+
+  def new
+    @car = Car.find(params[:car_id])
+    @repair = @car.repairs.build
+  end
+
+  def create
+    @car = Car.find(params[:car_id])
+    @repair = @car.repairs.build(repair_params)
+
+    if @repair.save
+      redirect_to repair_path(@repair), notice: "修理を登録しました"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def repair_params
+    params.require(:repair).permit(:description, :status)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'cars/show'
   get 'repairs/show'
   get 'customers/index'
   get 'customers/new'
