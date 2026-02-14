3c0d489cd2edcd1684836188c59aedde80ac94a4
RALPH <ralph@RALPHnoMacBook-Air.local>
2026-02-15 00:15:28 +0900

    customer detail

--- app/models/repair.rb
+++ app/models/repair.rb
@@ -2,4 +2,12 @@ class Repair < ApplicationRecord
   belongs_to :car
 
   enum status: { reception: 0, working: 1, completed: 2 }
+
+  def status_jp
+    {
+      "reception" => "受付中",
+      "working" => "作業中",
+      "completed" => "完了"
+    }[status]
+  end
 end
