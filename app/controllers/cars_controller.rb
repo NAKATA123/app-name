fe22ac4645f4b83da1ceaaa37aba03869c724cb9
RALPH <ralph@RALPHnoMacBook-Air.local>
2026-02-15 15:36:29 +0900

    車両・修理CRUD

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,4 +2,29 @@ class CarsController < ApplicationController
   def show
     @car = Car.find(params[:id])
   end
+
+  def edit
+    @car = Car.find(params[:id])
+  end
+
+  def update
+    @car = Car.find(params[:id])
+    if @car.update(car_params)
+      redirect_to car_path(@car), notice: "車両情報を更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @car = Car.find(params[:id])
+    @car.destroy
+    redirect_to customer_path(@car.customer), notice: "車両を削除しました"
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:car_model, :car_number, :customer_id)
+  end
 end
--- app/controllers/repairs_controller.rb
+++ app/controllers/repairs_controller.rb
@@ -23,6 +23,27 @@ class RepairsController < ApplicationController
     end
   end
 
+  def edit
+    @repair = Repair.find(params[:id])
+  end
+
+  def update
+    @repair = Repair.find(params[:id])
+    if @repair.update(repair_params)
+      redirect_to repair_path(@repair), notice: "修理情報を更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @repair = Repair.find(params[:id])
+    @repair.destroy
+    redirect_to repairs_path, notice: "修理を削除しました"
+  end
+
+
+
   private
 
   def repair_params
