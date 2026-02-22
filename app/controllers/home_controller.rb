c2a9d28db53c04018bc1c44048284d2ad436286f
RALPH <ralph@RALPHnoMacBook-Air.local>
2026-02-22 13:07:02 +0900

    修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
 class HomeController < ApplicationController
   def top
-    redirect_to login_path unless logged_in?
+    return unless logged_in?
+
+    today = Time.zone.today.all_day
+
+    @reception_count = Repair.where(status: :reception, created_at: today).count
+    @working_count   = Repair.where(status: :working,   created_at: today).count
+    @completed_count = Repair.where(status: :completed, created_at: today).count
+
+    # ここ修正
+    @loaned_count = Rental.where("end_date >= ?", Date.today).count
+    @available_count = LoanerCar.count - @loaned_count
   end
 end
